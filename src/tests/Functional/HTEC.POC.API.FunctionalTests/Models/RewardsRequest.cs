3c31cec42b728c3c984dbd41ff8d6e6b3d8a8be9
scottvaughanamido <scott.vaughan@amido.com>
2022-04-27 09:16:38 +0100

    Add code

--- src/tests/Functional/HTEC.POC.API.FunctionalTests/Models/RewardsRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace HTEC.POC.API.FunctionalTests.Models;
-
-public class RewardsRequest
-{
-    public string name { get; set; }
-    public string description { get; set; }
-    public bool enabled { get; set; }
-}
